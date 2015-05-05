c90f622e0ed7cdd3356b9fe1256750773d7fa153
salexzee <samwebb@codingtestsite.com>
2015-05-05 20:59:12 +0930

    adds association with job posts and users

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -15,11 +15,11 @@ class JobsController < ApplicationController
 	end
 
 	def new
-		@job = Job.new
+		@job = current_user.jobs.build
 	end
 
 	def create
-		@job = Job.new(jobs_params)
+		@job = current_user.jobs.build(jobs_params)
 
 		if @job.save
 			redirect_to @job
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505082607) do
+ActiveRecord::Schema.define(version: 20150505112003) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150505082607) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
