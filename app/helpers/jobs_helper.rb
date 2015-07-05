f6454fa465d39fd1ab921a2e32702e8243579566
salexzee <samwebb@codingtestsite.com>
2015-07-05 19:47:58 +0930

    Add placeholders to form when creating job posting

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,12 @@
 module JobsHelper
+	def random_job_title
+		titles = job_titles
+		title = titles[rand(titles.length)]
+	end
+
+	private
+
+	def job_titles
+		titles = ["Stock Room Manager", "Web Developer", "Party Planner", "Wedding Singer", "Creative Director", "Musician", "Redback Remover", "Photographer", "Technical Advisor", "Personal Assistant", "Mechanic", "Game Tester", "Dental Associate", "Stock Broker", "Social Media Editor", "Customer Success", "Web Engineer", "Sales Consultant"]	
+	end
 end
